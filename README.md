552dfb85ae30d5511b50db377df6d3385c302b10
Rodrigo B. de Oliveira <rbo@acm.org>
2014-02-21 13:58:36 -0300

    check file for errors upon each change

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-MightTable
-==========
+# MightTable
 
 Metascript plugin for LightTable
+
+# Installation
+
+1. Clone the project into LightTable plugins directory
+
+    cd ~/.config/LightTable/Plugins
+    git clone git@github.com:bamboo/MightTable.git
+    cd MightTable
+
+2. Install the meta-script package
+    
+    lein npm install
+ 
+3. Open src/lt/plugins/metascript.cljs in LightTable and execute the command "Editor: build file or project"
+
+# TODO
+
+* error display
+** show nested errors together with the main error
+** clicking the nested error goes to the referenced location
+
+* error navigation
+** goto next error
+** goto previous error
+
+* original error location
+** if there's an original location display icon to jump to that location
+*** to test write a macro that produces wrong code (coming from quoted code)
+
+* error pad
+** show all errors
+
+
---
+++ src/lt/plugins/metascript.cljs
@@ -0,0 +1,68 @@
+(ns lt.plugins.metascript
+  (:require [lt.objs.editor :as editor]
+            [lt.object :as object]
+            [lt.objs.files :as files]
+            [lt.objs.plugins :as plugins])
+  (:require-macros [lt.macros :refer [defui background behavior]]))
+
+(def metascript-path (files/join
+                      (or plugins/*plugin-dir* "/home/bamboo/.config/LightTable/plugins/MightTable")
+                      "node_modules/meta-script"))
+
+(def check-for-errors
+  (background
+   (fn [obj-id code metascript-path]
+     (let [meta ((js/require metascript-path))
+           compiler (. meta compilerFromString code)
+           ast (. compiler produceAst)
+           errors (.-errors compiler)
+           mjserror->clj (fn [e] {:message (.-message e) :line (.-line e)})
+           errors (vec (map mjserror->clj errors))]
+       (raise obj-id :mjs-hinted errors)))))
+
+
+(defui error-hint [errors]
+  [:div.hintwrapper
+   [:ul {:style "background-color: red;"}
+    (for [e errors]
+      [:li (:message e)])]])
+
+
+(defn preserving-scroll-location-of [this action]
+  (let [cm-ed (editor/->cm-ed this)
+        prev (.getScrollInfo cm-ed)]
+    (action)
+    (.scrollTo cm-ed (.-left prev) (.-top prev))))
+
+
+(defn remove-mjs-hints-from [this]
+  (doseq [hint (:mjs-hints @this)]
+    (editor/remove-line-widget this hint)))
+
+
+(defn update-hints [this hints]
+  (preserving-scroll-location-of
+   this
+   #(do
+      (remove-mjs-hints-from this)
+      (let [widgets {:mjs-hints
+                     (->> hints
+                          (group-by :line)
+                          (map (fn [[line hs]] (editor/line-widget this (dec line) (error-hint hs))))
+                          (doall))}]
+        (object/merge! this widgets)))))
+
+
+(object/behavior* ::on-change
+                  :debounce 750
+                  :type :user
+                  :desc "Metascript Errors: Check for Metascript errors on change"
+                  :triggers #{:change}
+                  :reaction (fn [this]
+                              (check-for-errors this (editor/->val this) metascript-path)))
+
+
+(object/behavior* ::inline-hints
+                  :triggers #{:mjs-hinted}
+                  :reaction (fn [this hints]
+                              (editor/operation (editor/->cm-ed this) #(update-hints this hints))))
