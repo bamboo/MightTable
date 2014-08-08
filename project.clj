60fe9acf8f46232c38a760f0927d551b4cb7537b
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-08 14:29:38 -0300

    meta-script@0.0.45

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject might-table "0.0.5"
+(defproject might-table "0.0.6"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-npm "0.3.0"]]
-  :node-dependencies [[meta-script "0.0.44"]])
+  :node-dependencies [[meta-script "0.0.45"]])
